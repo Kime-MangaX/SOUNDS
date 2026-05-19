6b12d8de9ad6da9628a43b1d9ea661d17cdafe86
Kime-MangaX <kimemanga@gmail.com>
2026-05-18 21:02:32 -0500

    Variables Karma

--- Assets/Scrips/Sistema Karmatico/Karma.cs
+++ Assets/Scrips/Sistema Karmatico/Karma.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class Karma : MonoBehaviour
 {
+    public int accionesBuenas = 2;
+    public int accionesNeutras = 1;
+    public int accionesMalas = 2;
+
+    public int karmaPlayer = 20;
+    public int karmaMax = 20;
+    public int karmaMin = 0;
+
     void Start()
     {
         
